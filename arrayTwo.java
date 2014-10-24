38d673e5ebe21aedc52a4ca4edb8d9ae21347418
Anne Celestino <hubwub@tagspace.me>
2014-10-24 13:17:45 -0500

    modThree and haveThree solved

--- arrayTwo.java
+++ arrayTwo.java
@@ -342,7 +342,31 @@ public class arrayTwo {
     // modThree({2, 4, 2, 5}) → true
 
     public boolean modThree(int[] nums) {
-    	
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] % 2 == 0 && nums[i + 1] % 2 == 0 && nums[i + 2] % 2 == 0) {
+                return true;
+            }
+            if (nums[i] % 2 == 1 && nums[i + 1] % 2 == 1 && nums[i + 2] % 2 == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Given an array of ints, return true if the value 3 appears in the array exactly 3 times, and no 3's are next to each other.
+
+    // haveThree({3, 1, 3, 1, 3}) → true
+    // haveThree({3, 1, 3, 3}) → false
+    // haveThree({3, 4, 3, 3, 4}) → false
+
+    public boolean haveThree(int[] nums) {
+    	int count = 0;
+    	for (int i = 0; i < nums.length-2; i++) {
+            if (nums[i] == 3 && nums[i + 2] == 3) {
+                count++;
+            }
+        }
+        return count == 2;
     }
 
 
