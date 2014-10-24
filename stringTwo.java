04e07126cf263c38e2ac7eeaa3863df2002a4d30
Anne Celestino <hubwub@tagspace.me>
2014-10-24 00:47:05 -0500

    hopefully everything is up to date

--- stringTwo.java
+++ stringTwo.java
@@ -296,16 +296,106 @@ public class stringTwo {
     // sameStarChar("*xa*az") → true
 
     public boolean sameStarChar(String str) {
-    	for(int i = 1; i < str.length()-1; i++){
-    		if(str.charAt(i) == '*'){
-    			if(str.charAt(i-1) == str.charAt(i+1)){
-    			} else{
-    				return false;
-    			}
-    		}
-    	}
-    	return true;
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (str.charAt(i) == '*') {
+                if (str.charAt(i - 1) == str.charAt(i + 1)) {
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
+
+
+    // Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'. Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".
+
+    // zipZap("zipXzap") → "zpXzp"
+    // zipZap("zopzop") → "zpzp"
+    // zipZap("zzzopzop") → "zzzpzp"
+
+    public String zipZap(String str) {
+        int len = str.length() - 2;
+        for (int i = 0; i < len; i++) {
+            if (str.charAt(i) == 'z' && str.charAt(i + 2) == 'p') {
+                str = str.substring(0, i + 1) + str.substring(i + 2, str.length());
+                len = str.length();
+            }
+        }
+
+        return str;
+    }
+
+
+    // Return a version of the given string, where for every star (*) in the string the star and the chars immediately to its left and right are gone. So "ab*cd" yields "ad" and "ab**cd" also yields "ad".
+
+    // starOut("ab*cd") → "ad"
+    // starOut("ab**cd") → "ad"
+    // starOut("sm*eilly") → "silly"
+
+    public String starOut(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != '*') {
+                if (i > 0 && str.charAt(i - 1) == '*') continue;
+                if (i + 1 < str.length() && str.charAt(i + 1) == '*') continue;
+                result += str.charAt(i);
+            }
+        }
+
+        return result;
+    }
+
+
+    // Given a string and a non-empty word string, return a version of the original String where all chars have been replaced by pluses ("+"), except for appearances of the word string which are preserved unchanged.
+
+    // plusOut("12xy34", "xy") → "++xy++"
+    // plusOut("12xy34", "1") → "1+++++"
+    // plusOut("12xy34xyabcxy", "xy") → "++xy++xy+++xy"
+
+    public String plusOut(String str, String word) {
+        int slen = str.length();
+        int wlen = word.length();
+        String result = "";
+
+        for (int i = 0; i < slen; i++) {
+            if (i + wlen - 1 < slen && str.substring(i, i + wlen).equals(word)) {
+                result += word;
+                i = i + wlen - 1;
+            } else {
+                result += "+";
+            }
+        }
+
+        return result;
+
+    }
+
+
+    // Given a string and a non-empty word string, return a string made of each char just before and just after every appearance of the word in the string. Ignore cases where there is no char before or after the word, and a char may be included twice if it is between two words.
+
+    // wordEnds("abcXY123XYijk", "XY") → "c13i"
+    // wordEnds("XY123XY", "XY") → "13"
+    // wordEnds("XY1XY", "XY") → "11
+
+    public String wordEnds(String str, String word) {
+        int slen = str.length();
+        int wlen = word.length();
+        String result = "";
+
+        for (int i = 0; i < slen - wlen + 1; i++) {
+            if (str.substring(i, i + wlen).equals(word)) {
+                if (i != 0) {
+                    result += str.substring(i - 1, i);
+                }
+                if (i + wlen != slen) {
+                    result += str.substring(i + wlen, i + wlen + 1);
+                }
+            }
+        }
+        return result;
+    }
+
 
 
 }
