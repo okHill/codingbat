d8093839294019cec900d15075a062ec8115cdc2
Anne Celestino <hubwub@tagspace.me>
2014-10-24 01:57:50 -0500

    started stringThree - countYZ

---
+++ stringThree.java
@@ -0,0 +1,19 @@
+public class stringThree {
+
+
+    // Given a string, count the number of words ending in 'y' or 'z' -- so the 'y' in "heavy" and the 'z' in "fez" count, but not the 'y' in "yellow" (not case sensitive). We'll say that a y or z is at the end of a word if there is not an alphabetic letter immediately following it. (Note: Character.isLetter(char) tests if a char is an alphabetic letter.)
+
+    // countYZ("fez day") → 2
+    // countYZ("day fez") → 2
+    // countYZ("day fyyyz") → 2
+
+    public int countYZ(String str) {
+    	int count = 0;
+    	for(int i = 0; i < str.length(); i++){
+    		
+    	}
+    	return count;
+    }
+
+
+}
